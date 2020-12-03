bd13f8ad16cba687a174dc41a21fb16f6da91262
mjcarignan <75333132+mjcarignan@users.noreply.github.com>
2020-12-03 09:53:49 -0500

    Update 20201203.py

--- 20201203.py
+++ 20201203.py
@@ -12,9 +12,8 @@ def readfile(filepath):
 		array2D.append(lineArray)
 	return array2D
 
-def countTrees(r,d):
+def countTrees(tbmap,r,d):
 	treecount = 0
-	tbmap = readfile("20201203_data")
 	rowpointer = 0
 	colpointer = 0
 	while rowpointer <= len(tbmap)-1 and colpointer <= len(tbmap[rowpointer]):
@@ -27,14 +26,18 @@ def countTrees(r,d):
 	return treecount
 
 def main():
-	slope1 = countTrees(1,1) 
-	slope2 = countTrees(3,1)
-	slope3 = countTrees(5,1)
-	slope4 = countTrees(7,1)
-	slope5 = countTrees(1,2)
+
+	tbmap = readfile("20201203_data")
+	slope1 = countTrees(tbmap,1,1) 
+	slope2 = countTrees(tbmap,3,1)
+	slope3 = countTrees(tbmap,5,1)
+	slope4 = countTrees(tbmap,7,1)
+	slope5 = countTrees(tbmap,1,2)
 
 	print (slope1*slope2*slope3*slope4*slope5)
 
 
-
+import time
+start_time = time.time() 
 main()
+print("Total time: %s milliseconds" % round((time.time() - start_time)*1000, 2))
