a800af9e0d9db10834d0e54f116b60adc77ed520
mjcarignan <75333132+mjcarignan@users.noreply.github.com>
2020-12-03 11:00:01 -0500

    refactored.

--- 20201203.py
+++ 20201203.py
@@ -26,16 +26,21 @@ def countTrees(tbmap,r,d):
 
 def main():
 	tbmap = readfile("20201203_data")
+	#print("File read: %s milliseconds" % round((time.time() - start_time)*1000, 3))
+
+	print("Day 3, part 1: " + str(countTrees(tbmap,3,1)))
+	#print("Time: %s milliseconds" % round((time.time() - start_time)*1000, 3))
+
 	slope1 = countTrees(tbmap,1,1) 
 	slope2 = countTrees(tbmap,3,1)
 	slope3 = countTrees(tbmap,5,1)
 	slope4 = countTrees(tbmap,7,1)
 	slope5 = countTrees(tbmap,1,2)
-
-	print (slope1*slope2*slope3*slope4*slope5)
+	print("Day 3, part 2: " + str((slope1*slope2*slope3*slope4*slope5)))
+	#print("Time: %s milliseconds" % round((time.time() - start_time)*1000, 3))
 
 
 import time
 start_time = time.time() 
 main()
-print("Total time: %s milliseconds" % round((time.time() - start_time)*1000, 2))
+print("Total time: %s milliseconds" % round((time.time() - start_time)*1000, 3))
