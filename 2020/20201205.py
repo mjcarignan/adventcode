7dfcec3483a473c2708369584eb434dacc74d5b0
Micheal <mcarignan@gmail.com>
2020-12-07 20:07:08 -0500

    Day 5, working.

--- 2020/20201205.py
+++ 2020/20201205.py
@@ -4,7 +4,7 @@ import pandas as pd
 def getSeatMap(numrows,seatsperrow):
 	row = []
 	for s in range(seatsperrow):
-		row.append('Empty')
+		row.append(0)
 	plane=[]
 	for r in range(numrows):
 		plane.append(row)
@@ -16,27 +16,57 @@ def readFile(file):
 		filedata = file_object.read().splitlines()
 		return(filedata)
 
+
+def getRow (bpass,rows):
+	start = 0
+	mid = ((rows) // 2) - 1
+	end = rows
+	for i in range(0,7):
+		if bpass[i] == 'F':
+			end = mid 
+			mid = (start+end) // 2
+		else:
+			start = mid 
+			mid = ((start + end) // 2)
+	return mid
+
+def getSeat (bpass,seatsperrow):
+	start = 0
+	mid = ((seatsperrow) // 2)
+	end = seatsperrow
+	for i in range(7,10):
+		if bpass[i] == 'L':
+			end = mid 
+			mid = (start+end) // 2
+		else:
+			start = mid 
+			mid = (start+end) // 2
+	return mid 
+
 def main():
 	#define plane
 	rows = 128
-	seatperrow = 8
-	dfplane = pd.DataFrame.from_records(getSeatMap(rows,seatperrow))
+	seatsperrow = 8
+	dfplane = pd.DataFrame.from_records(getSeatMap(rows,seatsperrow))
 
 	#fill seats based on boarding passes
 	boardingpasses = readFile('20201205_data')
-
+	row = 0
+	seat = 0
+	seatID = 0
+	count = 0
 	for i in range(len(boardingpasses)):
-		#get row
-		
-		break
+		row = getRow(boardingpasses[i],rows)
+		seat = getSeat(boardingpasses[i],seatsperrow)
+		dfplane.loc[row,seat] = ((row+1) * 8) + seat
+		count = i
 
-	dfplane.loc[0,0] = "Filled"
-	print(dfplane)
-
-
-	#answer day 1
-
+	#Part 1
+	pd.set_option('display.max_rows', None)
+	print(dfplane.to_numpy().max())
 	
+	#Part 2
+	print(dfplane)
 
 
 import time
--- 2020/test.py
+++ 2020/test.py
@@ -1,11 +1,3 @@
-
-
-import re 
-data = 'iyr:2016 hcl:#cfa07d eyr:2026 hgt:151cm pid:394185014 ecl:grn byr:1974'
-
-
-m = re.search("hgt:(.*?)' '", data)
-
-print(m)
-
+for i in range(0,9):
+	print(i)
 	
