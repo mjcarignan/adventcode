cf62770f584c0173caf17510a5e2c78abf8c166e
Micheal <mcarignan@gmail.com>
2020-12-07 23:05:20 -0500

    day 6 part complete

--- 2020/20201206.py
+++ 2020/20201206.py
@@ -1,5 +1,5 @@
 #20201206
-
+import collections
 #get data
 def readFile(file):
 	with open(file) as f:
@@ -8,15 +8,35 @@ def readFile(file):
 
 def main():
 	data = readFile('20201206_data')
+
+#Part 1
 	unique_yes_per_bgroup = 0
 	for bgroup in data.split("\n\n"):
-		passcount = bgroup.count("\n") + 1
+		passcount = bgroup.strip().count('\n') + 1
 		answers = set(bgroup)
 		if passcount > 1:
 			answers.remove("\n")
-		#print(bgroup  + '\n'+ str(len(answers)) + '\n')
 		unique_yes_per_bgroup += len(answers)
-	print(unique_yes_per_bgroup)
+	print("Part 1: " + str(unique_yes_per_bgroup))
+
+#Part 2
+	allyes = 0
+	count = [0] * 256
+	for bgroup in data.split("\n\n"):
+		#print(bgroup)
+		passcount = bgroup.strip().count('\n') + 1
+		#print("Pass: " + str(passcount))
+		bgroup = bgroup.replace("\n",'')
+		test = collections.Counter(bgroup) # collection count of each answer
+		for value in test.values():  
+			if value == passcount:
+				allyes += 1 
+		#print("Step: " + str(allyes))
+		
+	print("Part 2: " + str(allyes))
+#how many == the passcount of that group many are there?
+
+
 
 import time
 start_time = time.time() 
