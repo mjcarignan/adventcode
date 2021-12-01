0e86f584221d41ff5bc04340585acfc14f884ac3
mjcarignan <75333132+mjcarignan@users.noreply.github.com>
2021-12-01 18:13:41 -0500

    2021 first commit

---
+++ 2021/1201/source.py
@@ -0,0 +1,8 @@
+import time
+
+STARTTIME = time.time()
+
+#read file, get in to useable format
+file = open("C:\Users\mcarignan\OneDrive - New Century Health\Documents\adventcode2020\2021\1201\input", "r")
+#depths = [depth.strip() for depth in file.readlines()]
+#print(depths)
