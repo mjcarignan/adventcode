5f17edca5065bc35a6e78f9b5ce7126f08c800fa
mjcarignan <75333132+mjcarignan@users.noreply.github.com>
2021-12-01 20:15:28 -0500

    1201
    First challenge of 2021 done.

--- 2021/1201/source.py
+++ 2021/1201/source.py
@@ -1,8 +1,38 @@
 import time
 
-STARTTIME = time.time()
+startTime = time.time()
 
-#read file, get in to useable format
-file = open("C:\Users\mcarignan\OneDrive - New Century Health\Documents\adventcode2020\2021\1201\input", "r")
-#depths = [depth.strip() for depth in file.readlines()]
-#print(depths)
+
+#read file, get into useable format
+file = open('./2021/1201/inputfull.txt', 'r')
+depths = [int(depth.strip()) for depth in file.readlines()]
+
+#part 1
+compare = 0
+lastdepth = depths[0]
+for depth in depths:
+    if lastdepth < depth:
+        compare+=1
+    lastdepth = depth
+print('Answer part 1: ' + str(compare))
+
+
+#part 2
+length = len(depths)
+comparewindows = 0
+windows = []
+for i in range(length-2):
+    windows.append(depths[i]+depths[i+1]+depths[i+2])
+
+
+
+lastwindow = windows[0]
+for window in windows:
+
+    if lastwindow < window:
+        #print(str(window) + ' increase')
+        comparewindows+=1
+    lastwindow = window
+print('Answer part 2: ' + str(comparewindows))
+
+print("Total time: %s milliseconds" % round((time.time() - startTime)*1000, 3))
